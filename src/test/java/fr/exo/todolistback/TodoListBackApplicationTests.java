1cdf64f59ed42935203477edf849177fb7b3e0db
Taugry <teo.augry@limayrac.fr>
2023-09-25 16:23:39 +0200

    git init

---
+++ src/test/java/fr/exo/todolistback/TodoListBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.exo.todolistback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
